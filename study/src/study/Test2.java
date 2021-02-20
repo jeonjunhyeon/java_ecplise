2bad98eb19136bd25cf45d8da7fff6b894961377
kosmo_01 <kosmo_01@DESKTOP-3KEN0ST>
2021-02-20 15:27:44 +0900

    STUDY Time에 따로 연습해 본 결과
    작성자 : 전준현

--- study/src/study/Test2.java
+++ study/src/study/Test2.java
@@ -6,18 +6,15 @@ public class Test2 {
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int[] num = new int[10];
-		int sum = 0;
+		Test2 nums = new Test2();
 		
-		for(int i=0; i<num.length; i++) {
-			if(i<9) {
-				sum = sum + i;
+		for(int i=0;i<10;i++) {
+			for(int j=0;j<10;j++) {
+				System.out.println(i + " * " +j + " = " + (i*j) );
 			}
-			else {
-				break;
-			}
-			System.out.println(sum);
+		}
+		
+		sc.close();
 		}
 		
 	}
-}
